84f2bf47ed93cae071df9e45af879bb973d8bfc3
buhanzhe <buhanzhe@163.com>
2021-07-06 09:25:10 +0800

    初始化

---
+++ OnLive/src/main/java/com/frank/living/constant/Constants.java
@@ -0,0 +1,10 @@
+package com.frank.living.constant;
+
+public class Constants {
+
+    //broadcast of add client
+    public final static String ACTION_CLIENT_ADD = "action.client.ADD";
+    //broadcast of remove client
+    public final static String ACTION_CLIENT_REMOVE = "action.client.REMOVE";
+
+}
